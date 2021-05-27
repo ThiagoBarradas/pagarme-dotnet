fcd2174b715767525d66557f3e2c051f1f48c53c
Pedro Lucas Maia Ramos <pl.maia@hotmail.com>
2021-05-27 15:39:47 -0300

    :sparkles: Add Pix as a checkout payment method

---
+++ PagarMe/Models/Request/CreateCheckoutPixPaymentRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PagarMe.Models.Request
+{
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class CreateCheckoutPixPaymentRequest
+    {
+        public DateTime ExpiresAt { get; set; }
+    }
+}
---
+++ PagarMe/Models/Response/GetCheckoutPixPaymentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PagarMe.Models.Response
+{
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class GetCheckoutPixPaymentResponse
+    {
+        public DateTime ExpiresAt { get; set; }
+    }
+}
